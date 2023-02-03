dd1711ae6486ce3d0c4a4607195ffc9a37ddc37e
Domenico Paolo Picariello <picariellopaolo224@gmail.com>
2023-02-03 15:52:38 +0100

    Prima bozza del funzionamento dell'esercizio

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require_once __DIR__ . '/models/Movie.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,6 +15,12 @@
     <h1>
         PHP OOP 1
     </h1>
+    <div>
+        <?php
+        $movie1 = new Movie('Avengers', 'Tutti contro il crimine', 'green', '2 ore');
+        var_dump($movie1);
+        ?>
+    </div>
 </body>
 
 </html>
--- models/Movie.php
+++ models/Movie.php
@@ -2,4 +2,23 @@
 
 class Movie
 {
+    // CAMPI -->
+    public $title;
+    public $plot;
+    public $type;
+    public $duration;
+
+    public function __construct($_title, $_plot, $_type, $_duration)
+    {
+        $this->title = $_title;
+        $this->plot = $_plot;
+        $this->type = $_type;
+        $this->duration = $_duration;
+    }
+
+    // METHODS -->
+
+    // public function getTitle(){
+
+    // }
 }
