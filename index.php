7e29a884607615cfe540abb53de45ba41bee8982
Domenico Paolo Picariello <picariellopaolo224@gmail.com>
2023-02-03 16:06:35 +0100

    Creazione di più oggetti

--- index.php
+++ index.php
@@ -18,7 +18,10 @@ require_once __DIR__ . '/models/Movie.php';
     <div>
         <?php
         $movie1 = new Movie('Avengers', 'Tutti contro il crimine', 'green', '2 ore');
-        var_dump($movie1);
+        $movie2 = new Movie('Spiderman', 'Ragno contro il crimine', 'yellow', '1.5 ore');
+        $movie3 = new Movie('Batman', 'Pipistrello contro il crimine', 'red', '3 ore');
+        $movie4 = new Movie('Superman', 'Uomo contro il crimine', 'green', '1.5 ore');
+        var_dump($movie1, $movie2, $movie3, $movie4);
         ?>
     </div>
 </body>
